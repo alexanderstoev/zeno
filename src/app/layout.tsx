28891051057b7914f1e33da10a99fdd99f6ecec5
Alex Stoev <alex.stoev@gmail.com>
2025-09-11 11:53:45 +0300

    Project Setup and Dependencies Fixes #2 Added darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { DarkMode } from "@/components/ui/dark-mode";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -20,9 +22,17 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${geist.variable}`} suppressHydrationWarning>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <DarkMode />
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
